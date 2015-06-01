3715dd54c2924633e231f6725d3ce65b9dce6257
Gchaimke <Gchaimke@gmail.com>
2015-06-01 23:37:15 +0300

    Update jQuery_page_preload.js

--- preload/jQuery_page_preload.js
+++ preload/jQuery_page_preload.js
@@ -1,30 +1,3 @@
-<style>
-.loader{
-	background: none repeat scroll 0 0 #ffffff;
-	bottom: 0;
-	height: 100%;
-	left: 0;
-	position: fixed;
-	right: 0;
-	top:0;
-	width: 100%;
-	z-index: 9999;
-}
-.loaderInner{
-	background-image: url("../img/load.gif");
-	background-size: cover;
-	background-repeat: no-repeat;
-	background-position: center center;
-	background-color: #fff;
-	height: 60px;
-	width: 60px;
-	margin-top: -30px;
-	margin-right: -30px;
-	left: 50%;
-	right: 50%;
-	position: absolute;
-}
-</style>
 
 $(window).load(function(){
 $(".loader_inner").fadeOut();
